4f3c8d0b17c178aeca7d61ecbddbafa5e08ea208
Kanittan <100112532+Kanittan@users.noreply.github.com>
2022-02-21 05:46:57 -0500

    Add files via upload

---
+++ Lab7/Header Files/project_reversi_skeleton.h
@@ -0,0 +1,13 @@
+//
+//  Lab7Code.h
+//  Lab7 - Reversi
+//
+//  Created by Kanit Mongkonrattanachai on 23/3/2564 BE.
+//
+
+#ifndef Lab7Code_h
+#define Lab7Code_h
+
+#include <stdio.h>
+
+#endif /* Lab7Code_h */
